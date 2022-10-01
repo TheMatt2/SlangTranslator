9cfeead442abd0a88b1a62b3310e509476bc9466
TheMatt2 <TheMatt2@users.noreply.github.com>
2022-10-01 17:04:14 -0400

    Initial Version

--- slang_server.py
+++ slang_server.py
@@ -1,5 +1,4 @@
 import os
-import re
 import csv
 
 from flask import Flask, request, redirect, render_template
@@ -9,12 +8,29 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def home():
     # If this is a post, divert to the appropriate function
+    translated_slang = ""
     if request.form:
         action = request.form.get("action")
         if action == "load_data":
-            load_data()
-
+            content = request.form.get("content")
+            print("CONTENT", content)
+            translated_slang = translate_slang(content)
     return render_template("index.html")
 
+def translate_slang(content):
+    return content
+
+SLANG_FILE = "slang_words.csv"
+
+def load_slang():
+    slang_words = []
+    with open(SLANG_FILE) as f:
+        reader = csv.reader(f)
+        next(reader, None) # skip first line
+        for slang, definition in reader:
+            slang_words.append((slang, definition))
+    return slang_words
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    SLANG_WORDS = load_slang()
+    app.run(host = '0.0.0.0', debug = True)
